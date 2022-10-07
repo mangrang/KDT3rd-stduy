547e07c49a4913c5069fbebd5513b0748ce99a9e
mangrang <amole2315@naver.com>
2022-10-07 16:40:10 +0900

    출석체크 수정

--- 00_STUDY_GROUP/영훈/dailyCheck/dailyCheck.js
+++ 00_STUDY_GROUP/영훈/dailyCheck/dailyCheck.js
@@ -1,29 +1,43 @@
-let attend = ['김영훈', '오성인', '남궁수정', '염정호', '천현승', '강민성']
+const btn = document.querySelectorAll('.nameBtn')
+const sum = document.querySelector('.sum')
+const empty = document.querySelector('.empty')
+const total = document.getElementsByClassName('nameBtn')
+const allCo = document.querySelector('.w-btn-neon2')
+btn.forEach((button) => {
+  button.addEventListener('click', function ()   {
+    button.classList.toggle('w-btn-gra3')
+    
+    let names = document.getElementsByClassName('w-btn-gra3')
+    let totalNum = total.length;
+    let sumNum = names.length;
+    let emptyNum = totalNum-sumNum;
+    sum.innerText = sumNum;
+    empty.innerText = emptyNum
+  })
+
+})
 
 
-// for (i=1; i < attend.length; i++){
-//     const $form = document.querySelector('form')
-//     let $input = document.createElement('input')
-//     let $label = document.createElement('label')
-//     $input.setAttribute('type','checkbox')
-//     $input.setAttribute('id',`name${i}`)
-//     $input.setAttribute('name','attendance')
-//     $label.setAttribute('for',`name${i}`)
-//     $label.innerText=`${attend[i]}\n`
-//     $form.append($input)
-//     $form.append($label)
-// }
-
-const form = document.querySelector('form')
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-
-var checkbox = document.querySelector("input[name=attendance]");
-
-checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    console.log("Checkbox is checked..");
-  } else {
-    console.log("Checkbox is not checked..");
-  }
-});
+allCo.addEventListener('click', function ()   {
+  // button.classList.toggle('w-btn-gra3')
+  let names = document.getElementsByClassName('w-btn-gra3')
+  let totalNum = total.length;
+  let sumNum = names.length;
+  let emptyNum = totalNum-sumNum;
+  
+  if (emptyNum == 0){
+    btn.forEach((button) => {
+    button.classList.toggle('w-btn-gra3')
+    }
+ )}else{
+    btn.forEach((button) => {
+    button.classList.remove('w-btn-gra3')
+    button.classList.toggle('w-btn-gra3')
+  })}
+  totalNum = total.length;
+  sumNum = names.length;
+  emptyNum = totalNum-sumNum;
+  sum.innerText = sumNum;
+  empty.innerText = emptyNum
+  
+})
