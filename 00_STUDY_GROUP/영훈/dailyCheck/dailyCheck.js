42291f3510e4e57651b8e8c52959b98c5114d075
mangrang <amole2315@naver.com>
2022-10-11 09:28:31 +0900

    출석체크 수정

--- 00_STUDY_GROUP/영훈/dailyCheck/dailyCheck.js
+++ 00_STUDY_GROUP/영훈/dailyCheck/dailyCheck.js
@@ -3,11 +3,25 @@ const sum = document.querySelector('.sum')
 const empty = document.querySelector('.empty')
 const total = document.getElementsByClassName('nameBtn')
 const allCo = document.querySelector('.w-btn-neon2')
+const chgClr = document.querySelector('.chgClr')
+let randomBtn = ['w-btn-blue','w-btn-pink', 'w-btn-green', 'w-btn-brown', 'w-btn-gra3', 'w-btn-green2', 'w-btn-gray', 'w-btn-red', 'w-btn-skin', 'w-btn-yellow', '.w-btn-gra1']
+let nameBtn = 'w-btn-gra3'
+let num = Math.floor(Math.random()*4)
+
+chgClr.addEventListener('click', function(){
+  chgClr.classList.remove(randomBtn[num])
+  num = Math.floor(Math.random()*4)
+  nameBtn = randomBtn[num]
+  chgClr.classList.add(randomBtn[num])
+  
+})
+
 btn.forEach((button) => {
   button.addEventListener('click', function ()   {
-    button.classList.toggle('w-btn-gra3')
+  
+    button.classList.toggle(nameBtn)
     
-    let names = document.getElementsByClassName('w-btn-gra3')
+    let names = document.getElementsByClassName(nameBtn)
     let totalNum = total.length;
     let sumNum = names.length;
     let emptyNum = totalNum-sumNum;
@@ -19,21 +33,22 @@ btn.forEach((button) => {
 
 
 allCo.addEventListener('click', function ()   {
-  // button.classList.toggle('w-btn-gra3')
-  let names = document.getElementsByClassName('w-btn-gra3')
+  // button.classList.toggle(nameBtn)
+  let names = document.getElementsByClassName(nameBtn)
   let totalNum = total.length;
   let sumNum = names.length;
   let emptyNum = totalNum-sumNum;
   
   if (emptyNum == 0){
     btn.forEach((button) => {
-    button.classList.toggle('w-btn-gra3')
+    button.classList.toggle(nameBtn)
     }
  )}else{
     btn.forEach((button) => {
-    button.classList.remove('w-btn-gra3')
-    button.classList.toggle('w-btn-gra3')
+    button.classList.remove(nameBtn)
+    button.classList.toggle(nameBtn)
   })}
+
   totalNum = total.length;
   sumNum = names.length;
   emptyNum = totalNum-sumNum;
